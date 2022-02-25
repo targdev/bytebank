ca75c8fc0277021d87c479dc12565cfa8c614e16
Tarcisio Guimarães <79273548+targdev@users.noreply.github.com>
2022-02-25 19:39:42 -0300

    Update new-transfer.component.ts

--- src/app/new-transfer/new-transfer.component.ts
+++ src/app/new-transfer/new-transfer.component.ts
@@ -1,11 +1,12 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-    selector:'app-new-transfer',
-    templateUrl: './new-transfer.component.html',
-    styleUrls: ['./new-transfer.component.scss']
+  selector: 'app-new-transfer',
+  templateUrl: './new-transfer.component.html',
+  styleUrls: ['./new-transfer.component.scss'],
 })
-
 export class NewTransferComponent {
-
-}
+    transfer() {
+        console.log("solicited transfer")
+    }
+ }
