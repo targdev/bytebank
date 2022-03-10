12995bcad44379959688aa72d9e23ec31118bc6f
Tarcisio Guimarães <79273548+targdev@users.noreply.github.com>
2022-03-10 15:25:55 -0300

    Update extract.component.ts

--- src/app/extract/extract.component.ts
+++ src/app/extract/extract.component.ts
@@ -7,7 +7,7 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class ExtractComponent implements OnInit {
 
-  @Input() transferred: any;
+  @Input() transferred: any[] = [];
 
   constructor() { }
 
