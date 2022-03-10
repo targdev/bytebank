23977e6984ac1a9968be56c714428a5fbb93dc54
Tarcisio Guimarães <79273548+targdev@users.noreply.github.com>
2022-03-10 15:25:46 -0300

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,21 @@
-import { NgModule } from '@angular/core';
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NewTransferComponent } from './new-transfer/new-transfer.component';
 import { ExtractComponent } from './extract/extract.component';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+
+registerLocaleData(localePt, 'pt');
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NewTransferComponent,
-    ExtractComponent
-  ],
+  declarations: [AppComponent, NewTransferComponent, ExtractComponent],
 
-  imports: [
-    BrowserModule,
-    FormsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, FormsModule],
+  providers: [{ provide: LOCALE_ID, useValue: 'pt' },
+{ provide: DEFAULT_CURRENCY_CODE, useValue: "BRL"}],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
